ebd552a64a867b9e0840dfbf64ae55de0f9c1b1a
antonioriviezzo89 <riv.ant-89@hotmail.it>
2023-08-20 18:51:40 +0200

    ...

--- server.js
+++ server.js
@@ -40,8 +40,17 @@ app.post('/makeProxy', (req, res)=>{
             withCredentials: false,
             crossdomain: true,
             headers: {
-                "Accept": "*/*",  
-                "User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148", 
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1",
+                "Cache-Control": "max-age=0",                
               }
         })
         //.get(urlToBeProxy)
