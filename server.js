6626faf453c4d664f6776c9ac258142f5dc369c1
antonioriviezzo89 <riv.ant-89@hotmail.it>
2023-08-20 13:54:03 +0200

    ...

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var http = require('http');
 var axios = require('axios');
 
 const express = require('express');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 
@@ -9,19 +12,35 @@ app.use(cors());
 
 //rivaserver.c1.is
 
-/*
+
 //example url
 var url = `https://it.soccerway.com/a/block_competition_tables?`
     url += `block_id=page_competition_1_block_competition_tables_11`
     url += `&callback_params={"season_id":"23480", "round_id":"76333", "outgroup":"", "competition_id":"70", "new_design_callback":"1"}`
     url += `&action=changeTable&params={"type":"competition_wide_table"}`;   
-*/
+
 var porta = process.env.PORT || 3000;
-porta = 443;
+//porta = 443;
 console.log("porta: " + porta);
+
+app.use(morgan("dev"));
+
+
+
+app.use("/proxyData",
+    createProxyMiddleware({
+        target: url,
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/proxyData": "",
+        },
+    })
+);
 app.listen(porta, ()=>{
     console.log(`Sever is now listening ${porta}`);
 });
+
 
 //utils for restfull api of type post
 const bodyParser = require("body-parser");
